7f0608aacf99ca2ece586dd3d14aa77ea3a793c0
nurainwp2-cloud <nurainwp2@gmail.com>
2026-01-09 20:40:59 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+var icons = document.getElementById("icon")
+var body = document.getElementById("body")
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+
+}
+
+
