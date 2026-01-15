206e86075f4a82d9b487adc38dbcdd58728a2494
nurainwp2 <nurainwp2@gmail.com>
2026-01-15 12:03:43 +0600

    Songs added

--- script.js
+++ script.js
@@ -28,19 +28,52 @@ function showDate(){
 showDate()
 
 setInterval(showDate, 1000)
+let Name = document.getElementById("songtitle")
+let artist = document.getElementById("songArtist")
 
 
-function next(){
+function song2(){
     let song = document.getElementById("song") 
     song.src = "On The Flip - The Grey Room _ Density & Time.mp3"
     song.load();
     song.play();
+    Name.innerHTML = "On The Flip"
+    artist.innerHTML = "The Grey Room / Density & Time"
 
 }
-function prev(){
+function song1(){
     let song = document.getElementById("song") 
     song.src = "Go! - NEFFEX.mp3"
     song.play();
+    Name.innerHTML = "Go!"
+    artist.innerHTML = "NEFFEX"
 
 }
+function song3(){
+    let song = document.getElementById("song") 
+    song.src = "A Stroll - The Grey Room _ Density & Time.mp3"
+    song.play();
+    Name.innerHTML = "A Stroll"
+    artist.innerHTML = "The Grey Room / Density & Time"
+
+
+}
+function song4(){
+    let song = document.getElementById("song") 
+    song.src = "Cooked - The Grey Room _ Golden Palms.mp3"
+    song.play();
+    Name.innerHTML = "Cooked"
+    artist.innerHTML = "The Grey Room / Golden Palms"
+
+}
+function song5(){
+    let song = document.getElementById("song") 
+    song.src = "Claim To Fame - The Grey Room _ Clark Sims.mp3"
+    song.play();
+    Name.innerHTML = "Claim to Fame"
+    artist.innerHTML = "The Grey Room / Clark Sims"
+
+}
+
+
 
