e16fcaff60d9166e323210a02dca75b304f0235d
nurainwp2 <nurainwp2@gmail.com>
2026-01-17 10:57:29 +0600

    Random Password Script Done

--- script.js
+++ script.js
@@ -74,6 +74,35 @@ function song5(){
     artist.innerHTML = "The Grey Room / Clark Sims"
 
 }
+const passwordBox = document.getElementById("Pass");
+const length = 12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const num = "0123456789";
+const symbols = "@#$&^%_{(!";
+
+const allChars = upperCase + lowerCase + num + symbols;
+
+function createPassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += num[Math.floor(Math.random() * num.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+    
+    while (password.length < length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    
+    passwordBox.value = password;
+    
+}
+function copyPassword() {
+    passwordBox.select();
+    document.execCommand("copy");
+}
+
+
 
 
 
